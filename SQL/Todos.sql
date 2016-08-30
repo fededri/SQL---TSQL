d0b8b1dc3c4482b95a3bf485872c3c6eb85157c3
federico <fedetto94@hotmail.com>
2016-08-30 18:12:10 -0300

    ej 5

--- SQL/Todos.sql
+++ SQL/Todos.sql
@@ -25,11 +25,24 @@ order by prod_detalle asc
 
 
 --4) --DUDAS CON ESTE, recursividad en la composicion?
-select prod_codigo, prod_detalle, SUM(Co.comp_cantidad)
+select prod_codigo, prod_detalle, COUNT(Co.comp_componente), COUNT(ST.stoc_deposito) AS Deposito
 from Producto P LEFT JOIN Composicion Co ON prod_codigo = Co.comp_producto JOIN STOCK ST
 	on st.stoc_producto = P.prod_codigo
 	group by prod_codigo, prod_detalle
-	having AVG(st.stoc_cantidad)>1
+	having AVG(st.stoc_cantidad)>100
 
 
 --5)
+SELECT prod_codigo, prod_detalle,   SUM(item_cantidad) AS Egresos 
+FROM Producto, Item_Factura, Factura
+where prod_codigo  = item_producto and fact_numero = item_numero and
+		fact_tipo = item_tipo and fact_sucursal = item_sucursal and
+		YEAR(fact_fecha) = 2012
+group by prod_codigo, prod_detalle
+having SUM(item_cantidad) > (SELECT SUM(item_cantidad) FROM Item_Factura, Factura where
+								item_producto = prod_codigo and
+								item_numero = fact_numero and item_tipo = fact_tipo and
+								fact_sucursal = item_sucursal and YEAR(fact_fecha) = 2011)					 
+					 
+				
+				
